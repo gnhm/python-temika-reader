ec99c04ed37fb2a0102976580bae4c69adfa21bf
Guilherme Nettesheim <gn299@cam.ac.uk>
2021-05-24 14:59:02 +0100

    Remove test file

--- test.py
+++
@@ -1,5 +0,0 @@
-from pytmk import *
-
-m = Movie("/storage/NA_4/auto_flickering_07-05-2021_11:35:02/cell_0000.movie")
-#with open("/storage/NA_4/auto_flickering_07-05-2021_11:35:02/cell_0000.movie", 'rb') as f:
-#	print(f.read(200))
