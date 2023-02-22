9df24b20fbdbba0a90f5bc9e40f5a8b88eaba2d8
jamie <jsm89@cantab.ac.uk>
2023-02-22 17:06:38 +0100

    add local changes to jamie branch

--- write_movie.py
+++ write_movie.py
@@ -5,40 +5,45 @@ from itertools import chain
 import matplotlib.pyplot as plt
 import numpy as np
 
-mule_f = '/home/gn299/code/python-temika-reader/mule.movie'
+mule_f = "/home/gn299/code/python-temika-reader/mule.movie"
+
 
 def write_frames_to_movie(new_file, frames):
-	m = Movie(mule_f)
+    m = Movie(mule_f)
 
-	first_frame = next(frames)
-	plt.imshow(first_frame)
-	new_size_y, new_size_x = first_frame.shape #Yes, the order is correct. y first, then x.
-	length_data = new_size_y*new_size_x*2
-        
-	frames_g = chain((xx for xx in [first_frame]), frames)
+    first_frame = next(frames)
+    plt.imshow(first_frame)
+    (
+        new_size_y,
+        new_size_x,
+    ) = first_frame.shape  # Yes, the order is correct. y first, then x.
+    length_data = new_size_y * new_size_x * 2
 
-	with open(new_file, 'wb') as new_movie:
-		new_movie.seek(0)
-		m.file.seek(0)
+    frames_g = chain((xx for xx in [first_frame]), frames)
 
-		#Write the movie header
-		new_movie.write(m.file.read(m.movie_header_index))
+    with open(new_file, "wb") as new_movie:
+        new_movie.seek(0)
+        m.file.seek(0)
 
-		for i,frame in enumerate(frames_g):
-			m.file.seek(m.movie_header_index)
+        # Write the movie header
+        new_movie.write(m.file.read(m.movie_header_index))
 
-			new_movie.write(m.file.read(20))
+        for i, frame in enumerate(frames_g):
+            m.file.seek(m.movie_header_index)
 
-			new_movie.write(struct.pack('I', length_data)) 
-			m.file.seek(4, 1)
+            new_movie.write(m.file.read(20))
 
-			new_movie.write(m.file.read(m.movie_header_index + 64 - m.file.tell()))
-			new_movie.write(struct.pack('I', new_size_x))
-			new_movie.write(struct.pack('I', new_size_y))
-			m.file.seek(8, 1)
-			
-			new_movie.write(m.file.read(m.movie_header_index + m.length_header - m.file.tell())) #Write the frame header
+            new_movie.write(struct.pack("I", length_data))
+            m.file.seek(4, 1)
 
-			s = tuple(frame.flatten())
-			new_movie.write(struct.pack('>' + (length_data//2)*"H", *s))
-			
+            new_movie.write(m.file.read(m.movie_header_index + 64 - m.file.tell()))
+            new_movie.write(struct.pack("I", new_size_x))
+            new_movie.write(struct.pack("I", new_size_y))
+            m.file.seek(8, 1)
+
+            new_movie.write(
+                m.file.read(m.movie_header_index + m.length_header - m.file.tell())
+            )  # Write the frame header
+
+            s = tuple(frame.flatten())
+            new_movie.write(struct.pack(">" + (length_data // 2) * "H", *s))
